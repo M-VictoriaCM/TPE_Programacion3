ebe8093a58ddb2ee9d806523806337a07446a297
M Victoria <mvittoria7@gmail.com>
2023-06-24 21:58:52 -0300

    programa completo

--- src/ServicioBFS.java
+++ src/ServicioBFS.java
@@ -1,59 +1,52 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class ServicioBFS {
     private Grafo<?> grafo;
+    private Set<Integer>visitado;
+    private List<Integer> resultado;
 
     public ServicioBFS(Grafo<?> grafo) {
         this.grafo = grafo;
+        this.visitado= new HashSet<>();
+        this.resultado = new ArrayList<>();
     }
 
     public List<Integer> bfsForest() {
-        // Resolver BFS
-        return new ArrayList<>();
-    }
-	/*
-	public class ServicioBFS {
-    private Grafo<?> grafo;
-
-    public ServicioBFS(Grafo<?> grafo) {
-        this.grafo = grafo;
-    }
-
-    public List<Integer> bfsForest() {
-        List<Integer> resultado = new ArrayList<>();
-        Set<Integer> visitados = new HashSet<>();
-        Queue<Integer> cola = new LinkedList<>();
-
         Iterator<Integer> vertices = grafo.obtenerVertices();
-        while (vertices.hasNext()) {
+        while(vertices.hasNext()){
             int vertice = vertices.next();
-            if (!visitados.contains(vertice)) {
-                bfs(vertice, visitados, cola, resultado);
+            if(!visitado.contains(vertice)){
+                bfs(vertice);
             }
         }
 
         return resultado;
     }
 
-    private void bfs(int vertice, Set<Integer> visitados, Queue<Integer> cola, List<Integer> resultado) {
-        visitados.add(vertice);
-        cola.offer(vertice);
+    private void bfs(int vertice) {
+        LinkedList<Integer> cola = new LinkedList<>();
+        cola.add(vertice);
+        visitado.add(vertice);
 
-        while (!cola.isEmpty()) {
-            int actual = cola.poll();
-            resultado.add(actual);
+        while(!cola.isEmpty()) {
+            int grafoActual = cola.removeFirst();
+            resultado.add(grafoActual);
 
-            Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(actual);
-            while (adyacentes.hasNext()) {
+            Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(grafoActual);
+            while(adyacentes.hasNext()){
                 int adyacente = adyacentes.next();
-                if (!visitados.contains(adyacente)) {
-                    visitados.add(adyacente);
-                    cola.offer(adyacente);
+                if(!visitado.contains(adyacente)){
+                    cola.add(adyacente);
+                    visitado.add(adyacente);
                 }
             }
         }
+
     }
-}
-	*/
+
 }
--- src/ServicioDFS.java
+++ src/ServicioDFS.java
@@ -1,52 +1,41 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class ServicioDFS {
     private Grafo<?> grafo;
+    private Set<Integer>visitado;
+    private List<Integer> resultado;
 
     public ServicioDFS(Grafo<?> grafo) {
         this.grafo = grafo;
+        this.visitado= new HashSet<>();
+        this.resultado = new ArrayList<>();
     }
 
     public List<Integer> dfsForest() {
-        // Resolver DFS
-        return new ArrayList<>();
-    }
-	/*
-	public class DFS<T> {
-    private Grafo<T> grafo;
-
-    public DFS(Grafo<T> grafo) {
-        this.grafo = grafo;
-    }
-
-    public List<Integer> dfsForest() {
-        List<Integer> resultado = new ArrayList<>();
-        Set<Integer> visitados = new HashSet<>();
-
-        Iterator<Integer> vertices = grafo.obtenerVertices();
-        while (vertices.hasNext()) {
+        Iterator<Integer>vertices = grafo.obtenerVertices();
+        while(vertices.hasNext()){
             int vertice = vertices.next();
-            if (!visitados.contains(vertice)) {
-                dfs(vertice, visitados, resultado);
+            if(!visitado.contains(vertice)){
+                dfs(vertice);
             }
         }
-
         return resultado;
     }
 
-    private void dfs(int vertice, Set<Integer> visitados, List<Integer> resultado) {
-        visitados.add(vertice);
+    private void dfs(int vertice) {
+        visitado.add(vertice);
         resultado.add(vertice);
+        Iterator<Integer>adyacentes = grafo.obtenerAdyacentes(vertice);
+        while(adyacentes.hasNext()){
+            int  adyacente = adyacentes.next();
 
-        Iterator<Integer> adyacentes = grafo.obtenerAdyacentes(vertice);
-        while (adyacentes.hasNext()) {
-            int adyacente = adyacentes.next();
-            if (!visitados.contains(adyacente)) {
-                dfs(adyacente, visitados, resultado);
+            if(!visitado.contains(adyacente)){
+                dfs(adyacente);
             }
         }
     }
-}
-	*/
 }
